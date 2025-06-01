606bf6f89fe370a97b62a06cd200604bc565a91b
shintaro-usui <st.ar.us0211@gmail.com>
2025-06-01 10:01:47 +0900

    fix: transfer react-script to vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,45 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { AuthProvider } from './contexts/AuthContext';
+import Layout from './components/layout/Layout';
 import LoginForm from './components/auth/LoginForm';
 import RegisterForm from './components/auth/RegisterForm';
 import PrivateRoute from './components/auth/PrivateRoute';
 import ArticleList from './components/articles/ArticleList';
 import ArticleDetail from './components/articles/ArticleDetail';
 import CreateArticle from './components/articles/CreateArticle';
-import Layout from './components/layout/Layout';
+
+const theme = createTheme();
 
 const App: React.FC = () => {
-    return (
-        <Layout>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <Router>
+            <Layout>
             <Routes>
+                <Route path="/" element={<ArticleList />} />
                 <Route path="/login" element={<LoginForm />} />
                 <Route path="/register" element={<RegisterForm />} />
-                <Route path="/" element={<ArticleList />} />
                 <Route path="/articles/:id" element={<ArticleDetail />} />
                 <Route
-                    path="/articles/create"
-                    element={
-                        <PrivateRoute>
-                            <CreateArticle />
-                        </PrivateRoute>
-                    }
+                path="/articles/create"
+                element={
+                    <PrivateRoute>
+                    <CreateArticle />
+                    </PrivateRoute>
+                }
                 />
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
-        </Layout>
-    );
+            </Layout>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
+  );
 };
 
 export default App;
